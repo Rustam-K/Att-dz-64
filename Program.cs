79794154ab53dbeb852d11bcfaf1ba18e084bc2f
Rustam <autocentr@list.ru>
2023-11-24 23:02:22 +0500

    аттестация

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿   Console.Write("Введите значение M: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите значение N: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"Чётные числа от {m} до {n}:");
+    evenNumbers(m, n);
+
+void evenNumbers(int start, int end)
+{
+        if (start > end)
+    {
+        return;
+    }
+    if (start % 2 == 0)
+    {
+        Console.WriteLine(start);
+    }
+
+ 
+    evenNumbers(start + 1, end);
+}
