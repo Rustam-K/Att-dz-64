3e631e52298ab46b7d024a1805db4ac216374528
Rustam <autocentr@list.ru>
2023-11-25 14:11:22 +0500

    1

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
-﻿   Console.Write("Введите значение M: ");
+﻿ //Задача 64: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+//M = 1; N = 5 -> "2, 4"
+//M = 4; N = 8 -> "4, 6, 8"
+
+ 
+   Console.Write("Введите значение M: ");
     int m = Convert.ToInt32(Console.ReadLine());
 
     Console.Write("Введите значение N: ");
     int n = Convert.ToInt32(Console.ReadLine());
 
     Console.WriteLine($"Чётные числа от {m} до {n}:");
-    evenNumbers(m, n);
+    evenNumbers(m, n); -m
 
 void evenNumbers(int start, int end)
 {
